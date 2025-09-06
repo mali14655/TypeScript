343074bd5d3a7d027e5c6e3a4b1568e1c27ab29b
Muhammad Ali <mali146643@gmail.com>
2025-09-06 14:43:14 +0500

    types

---
+++ Day2/dist/app.js
@@ -0,0 +1,16 @@
+// types
+const myName = "muhammad ali";
+let userId = 10.4;
+let isloggedIn = true;
+console.log(myName, userId, isloggedIn);
+// Arrays
+let names = [
+    'ali', 'asim', 'ahamad'
+];
+// or
+let names2 = [
+    'ali', 'asim', 'ahamad'
+];
+console.log(names, names);
+let numbers = [1, 2, 3, 4, 5, 5];
+console.log(numbers);
---
+++ Day2/src/app.ts
@@ -0,0 +1,24 @@
+
+// types
+
+const myName:string="muhammad ali";
+let userId:number=10.4;
+let isloggedIn:boolean=true;
+
+console.log(myName,userId,isloggedIn);
+
+// Arrays
+
+let names:string[]=[
+    'ali','asim','ahamad'
+]
+
+// or
+let names2:Array<string>=[
+    'ali','asim','ahamad'
+]
+
+console.log(names,names);
+
+let numbers:Array<number>=[1,2,3,4,5,5];
+console.log(numbers)
